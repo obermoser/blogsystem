3853ae55ba6b05aa34f50c1ce53be76395bdee70
Bernhard <bernhard.obermoser@icloud.com>
2025-12-09 16:12:28 +0100

    feat: implement getById query to fetch post details with image URL resolution

--- app/(main)/blog/[postId]/page.tsx
+++ app/(main)/blog/[postId]/page.tsx
@@ -1,8 +1,17 @@
 import { buttonVariants } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { fetchQuery } from 'convex/nextjs';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
 
-const PostIdPage = () => {
+interface PostIdProps {
+  params: Promise<{ postId: Id<'posts'> }>;
+}
+
+const PostIdPage = async ({ params }: PostIdProps) => {
+  const { postId } = await params;
+  const data = await fetchQuery(api.posts.getById, { postId: postId });
   return (
     <div className="max-w-3xl mx-auto py-8 px-4 animate-in fade-in duration-500 relative">
       <Link className={buttonVariants({ variant: 'ghost' })} href="/blog" prefetch>
@@ -10,7 +19,9 @@ const PostIdPage = () => {
         Back to Blog
       </Link>
 
-      <div className="relative w-full h-[400px] mb-8 rounded-xl overflow-hidden shadow-sm">FFF</div>
+      <div className="relative w-full h-[400px] mb-8 rounded-xl overflow-hidden shadow-sm">
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
     </div>
   );
 };
